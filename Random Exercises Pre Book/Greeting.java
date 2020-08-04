25448c6a7866bef54935ef3f84cd68400efedccc
Chyna Sarmiento <45527870+gleislythine@users.noreply.github.com>
2020-08-04 21:18:46 +0800

    Delete Greeting.java

--- Random Exercises Pre Book/Greeting.java
+++
@@ -1,8 +0,0 @@
-public class Greeting {
-    public static void main(String [] args) {
-    	System.out.print("Hej! \t");
-    	System.out.print("\\Pryvit!\\ \t");
-    	System.out.print("Annyeong! \n");
-    	System.out.println("\"I'm not linguistic.\"");
-    }
-}

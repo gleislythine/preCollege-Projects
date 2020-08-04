f0dc2b996c5fea5723ce418e0414ef600beba7db
Chyna Sarmiento <45527870+gleislythine@users.noreply.github.com>
2020-08-04 21:35:35 +0800

    Create tenName.java

---
+++ Year 10/Chapter 6/Random Exercises/tenName.java
@@ -0,0 +1,22 @@
+public class tenName {
+    public static void main(String[] args) {
+    	int x = 0, y = 0;
+    	while(x < 10){
+    		x++;
+    		System.out.println(x + ". " + "Chyna Sarmiento");
+    	}
+    	
+    	System.out.print("\n");
+    	
+    	do{
+    		y += 1;
+    		System.out.println(y + ". " + "Chyna Sarmiento");
+    	} while(y < 10);
+    	
+    	System.out.print("\n");
+    	
+    	for(int i = 1; i <= 10; i++){
+    		System.out.println(i + ". " + "Chyna Sarmiento");
+    	}
+    }
+}

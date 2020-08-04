b380db3d25f4b3cf9d742b55704cc7513234c591
Chyna Sarmiento <45527870+gleislythine@users.noreply.github.com>
2020-08-04 21:57:50 +0800

    Create LunchOrder.java

---
+++ Year 11/Chapter 08/Exercises/Chapter 08 Exercise 03 - Lunch Order/LunchOrder.java
@@ -0,0 +1,12 @@
+/**
+ * Chyna Sarmiento
+ * 25 8 2017
+ * P1: AP Computer Science
+ * 
+ * Exercise 03: Lunch Order
+ */
+
+public class e03_LunchOrder{
+    public static void main(String[] args){
+    }
+}

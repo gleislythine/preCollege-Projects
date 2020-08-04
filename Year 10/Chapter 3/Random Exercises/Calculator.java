8d4fccb1f86675e8f5f4e24177f8fec5f0ebfef5
Chyna Sarmiento <45527870+gleislythine@users.noreply.github.com>
2020-08-04 21:21:55 +0800

    Add files via upload

---
+++ Year 10/Chapter 3/Random Exercises/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+    public static void main(String [] Args) {
+    	System.out.println("6" + " + " + "3" + " = " + (6 + 3));
+    	System.out.println("6" + " - " + "3" + " = " + (6 - 3));
+    	System.out.println("6" + " * " + "3" + " = " + (6 * 3));
+    	System.out.println("6" + " / " + "3" + " = " + (6 / 3));
+    }
+}
---
+++ Year 10/Chapter 3/Random Exercises/FormattingOutput.java
@@ -0,0 +1,9 @@
+public class FormattingOutput {
+    public  static void main(String [] Args) {
+    	System.out.format("%-10s %8s %8s", "Team", "Wins", "Losses\n");
+    	System.out.format("%-10s %8s %8s", "Jaguar", "10", "5\n");
+    	System.out.format("%-10s %8s %8s", "Cheetahs", "14", "1\n");
+    	System.out.format("%-10s %8s %8s", "Panthers", "8", "7\n");
+    	System.out.format("%-10s %8s %8s", "Penguins", "4", "11\n");
+    }
+}
